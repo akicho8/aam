ff2ee41cd7d6b3c56c162b426fe18082a739e5de
akicho8 <akicho8@gmail.com>
2017-01-22 21:31:38 +0900

    表示改善

--- examples/0100_example.rb
+++ examples/0100_example.rb
@@ -13,7 +13,7 @@ ActiveRecord::Schema.define do
 
     create_table :articles do |t|
       t.belongs_to :user, index: true
-      t.belongs_to :xxx, :polymorphic => true, :index => false # Rails5からindex:trueがデフォルトになっているため
+      t.belongs_to :foo, :polymorphic => true, :index => false # Rails5からindex:trueがデフォルトになっているため
     end
 
     create_table :blogs do |t|
@@ -24,20 +24,20 @@ end
 
 class User < ActiveRecord::Base
   has_many :articles
-  has_one :article, :as => :xxx
+  has_one :article, :as => :foo
 end
 
 class Article < ActiveRecord::Base
   belongs_to :user
-  belongs_to :xxx, :polymorphic => true
+  belongs_to :foo, :polymorphic => true
 end
 
 class Blog < ActiveRecord::Base
-  has_many :sub_articles, :foreign_key => :xxx_id
+  has_many :sub_articles, :foreign_key => :foo_id
 end
 
 class SubArticle < Article
-  belongs_to :blog, :class_name => "Blog", :foreign_key => :xxx_id
+  belongs_to :blog, :class_name => "Blog", :foreign_key => :foo_id
 end
 
 Aam.logger = nil
@@ -49,7 +49,49 @@ if true
   I18n.backend.store_translations :ja, {:activerecord => {:models => {"sub_article" => "なんとか"} }, :attributes => {"user_id" => "ユーザー"}}
 end
 
+puts Aam::SchemaInfoGenerator.new(User).generate
+puts Aam::SchemaInfoGenerator.new(Article).generate
+puts Aam::SchemaInfoGenerator.new(Blog).generate
 puts Aam::SchemaInfoGenerator.new(SubArticle).generate
+
+# >> # == Schema Information ==
+# >> #
+# >> # Userテーブル (users as User)
+# >> #
+# >> # +----------+------+---------+-------------+------+-------+
+# >> # | カラム名 | 意味 | タイプ  | 属性        | 参照 | INDEX |
+# >> # +----------+------+---------+-------------+------+-------+
+# >> # | id       | Id   | integer | NOT NULL PK |      |       |
+# >> # | name     | Name | string  |             |      | A!    |
+# >> # | flag     | Flag | boolean | DEFAULT(f)  |      |       |
+# >> # +----------+------+---------+-------------+------+-------+
+# >> # == Schema Information ==
+# >> #
+# >> # Articleテーブル (articles as Article)
+# >> #
+# >> # +----------+----------+---------+-------------+-----------------------+-------+
+# >> # | カラム名 | 意味     | タイプ  | 属性        | 参照                  | INDEX |
+# >> # +----------+----------+---------+-------------+-----------------------+-------+
+# >> # | id       | Id       | integer | NOT NULL PK |                       |       |
+# >> # | user_id  | ユーザー | integer |             | => User#id            | A     |
+# >> # | foo_type | Foo type | string  |             | モデル名(polymorphic) |       |
+# >> # | foo_id   | Foo      | integer |             | => (foo_type)#id      |       |
+# >> # +----------+----------+---------+-------------+-----------------------+-------+
+# >> #
+# >> #- 備考 -------------------------------------------------------------------------
+# >> # ・Article モデルは User モデルから has_many :articles されています。
+# >> # ・【警告:インデックス欠如】create_articles マイグレーションに add_index :articles, [:foo_id, :foo_type] を追加してください
+# >> #--------------------------------------------------------------------------------
+# >> # == Schema Information ==
+# >> #
+# >> # Blogテーブル (blogs as Blog)
+# >> #
+# >> # +----------+------+---------+-------------+------+-------+
+# >> # | カラム名 | 意味 | タイプ  | 属性        | 参照 | INDEX |
+# >> # +----------+------+---------+-------------+------+-------+
+# >> # | id       | Id   | integer | NOT NULL PK |      |       |
+# >> # | name     | Name | string  |             |      |       |
+# >> # +----------+------+---------+-------------+------+-------+
 # >> # == Schema Information ==
 # >> #
 # >> # なんとかテーブル (articles as SubArticle)
@@ -59,12 +101,12 @@ puts Aam::SchemaInfoGenerator.new(SubArticle).generate
 # >> # +----------+----------+---------+-------------+--------------------------------------+-------+
 # >> # | id       | Id       | integer | NOT NULL PK |                                      |       |
 # >> # | user_id  | ユーザー | integer |             | => User#id                           | A     |
-# >> # | xxx_type | Xxx type | string  |             | モデル名(polymorphic)                |       |
-# >> # | xxx_id   | Xxx      | integer |             | :blog => Blog#id と => (xxx_type)#id |       |
+# >> # | foo_type | Foo type | string  |             | モデル名(polymorphic)                |       |
+# >> # | foo_id   | Foo      | integer |             | :blog => Blog#id と => (foo_type)#id |       |
 # >> # +----------+----------+---------+-------------+--------------------------------------+-------+
 # >> #
 # >> #- 備考 -------------------------------------------------------------------------
 # >> # ・SubArticle モデルは User モデルから has_many :articles されています。
-# >> # ・【警告:インデックス欠如】create_articles マイグレーションに add_index :articles, [:xxx_id, :xxx_type] を追加してください
-# >> # ・SubArticle モデルは Blog モデルから has_many :sub_articles, :foreign_key => :xxx_id されています。
+# >> # ・【警告:インデックス欠如】create_articles マイグレーションに add_index :articles, [:foo_id, :foo_type] を追加してください
+# >> # ・SubArticle モデルは Blog モデルから has_many :sub_articles, :foreign_key => :foo_id されています。
 # >> #--------------------------------------------------------------------------------
